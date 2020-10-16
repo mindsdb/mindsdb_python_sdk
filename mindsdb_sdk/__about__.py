98cf867a45fdef6215829bda01f48a38c70149e9
George3d6 <george@cerebralab.com>
2020-10-17 02:42:42 +0300

    renamed to sdk to be in line with the javascript version, updated descriptions

--- mindsdb_sdk/__about__.py
+++ mindsdb_sdk/__about__.py
@@ -1,10 +1,10 @@
-__title__ = 'mindsdb_client'
-__package_name__ = 'mindsdb_client'
-__version__ = '0.9.9'
-__description__ = "MindsDB client, provides an SDK to connect and use the mindsdb server capabilities to mindsdb native python library"
+__title__ = 'mindsdb_sdk'
+__package_name__ = 'mindsdb_sdk'
+__version__ = '0.10.0'
+__description__ = "MindsDB Python SDK, provides an SDK to use a remote mindsdb instance"
 __email__ = "jorge@mindsdb.com"
 __author__ = 'MindsDB Inc'
 __github__ = 'https://github.com/mindsdb/mindsdb_python_sdk'
-__pypi__ = 'https://pypi.org/project/mindsdb-client/'
+__pypi__ = 'https://pypi.org/project/mindsdb-sdk/'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2020- mindsdb'
--- mindsdb_sdk/__init__.py
+++ mindsdb_sdk/__init__.py
@@ -1,5 +1,5 @@
 import sys
-if sys.version_info < (3,6):
-    sys.exit('Sorry, For MindsDB Client does not support python < 3.6')
+if sys.version_info < (3,5):
+    raise EnvironmentError('Please install a python version >= 3.5 to use this library')
 
 from mindsdb_client.classes.mindsdb import MindsDB
