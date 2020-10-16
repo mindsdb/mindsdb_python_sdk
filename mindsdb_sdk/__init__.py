042c0c4196a5808c376b1ef725476cb9d19a7520
George3d6 <george@cerebralab.com>
2020-10-17 02:43:48 +0300

    updated init message and required version

--- mindsdb_sdk/__init__.py
+++ mindsdb_sdk/__init__.py
@@ -2,4 +2,4 @@ import sys
 if sys.version_info < (3,5):
     raise EnvironmentError('Please install a python version >= 3.5 to use this library')
 
-from mindsdb_client.classes.mindsdb import MindsDB
+from mindsdb_sdk.classes.mindsdb import MindsDB
