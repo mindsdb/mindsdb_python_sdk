e5c0cf048aa134c46198df64d3c1156c6389f39f
George3d6 <george@cerebralab.com>
2020-11-05 21:01:03 +0200

    fixed max comment nr 6

--- mindsdb_sdk/__init__.py
+++ mindsdb_sdk/__init__.py
@@ -1,6 +1,6 @@
 import sys
 if sys.version_info < (3,5):
-    raise EnvironmentError('Please install a python version >= 3.5 to use this library')
+    raise EnvironmentError('Please install a python version >= 3.6 to use this library')
 
 from mindsdb_sdk.classes.sdk import SDK
 from mindsdb_sdk.classes.pandas_acessor import AutoML, auto_ml_config
