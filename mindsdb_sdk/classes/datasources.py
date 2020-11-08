f966dd562e8c085e16c5fefde38f78887a2c09bb
George3d6 <george@cerebralab.com>
2020-11-08 18:46:31 +0200

    added manifest

--- mindsdb_sdk/classes/datasources.py
+++ mindsdb_sdk/classes/datasources.py
@@ -10,8 +10,6 @@ class DataSource():
     def get_info(self):
         return self._proxy.get(f'/datasources/{self.name}')
 
-
-
     @property
     def analysis(self, wait_seconds=360):
         if self._analysis is None:
