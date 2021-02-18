a49a8b60f4244bf20c9c4ec4f75bbca4b9d67021
Ilia Tsyplenkov <ilia-tsyplenkov@users.noreply.github.com>
2021-02-18 12:41:59 +0300

    Fix integration tests (#22)
    * Fix integration tests

--- tests/test_integrations.py
+++ tests/test_integrations.py
@@ -32,10 +32,10 @@ class TestDatasources(unittest.TestCase):
             time.sleep(40)
         cls.sdk = SDK('http://localhost:47334')
         cls.integrations = cls.sdk.integrations
-        cls.cloud_sdk = SDK('https://cloud.mindsdb.com',
-                            user='george@cerebralab.com',
-                            password='12345678')
-        cls.cloud_integrations = cls.cloud_sdk.integrations
+        # cls.cloud_sdk = SDK('https://cloud.mindsdb.com',
+        #                     user='george@cerebralab.com',
+        #                     password='12345678')
+        # cls.cloud_integrations = cls.cloud_sdk.integrations
 
         # need to have a uniq name for each launch to avoid race condition in cloud
         # mongo_darwin_python_3.8
@@ -66,8 +66,8 @@ class TestDatasources(unittest.TestCase):
     def test_1_list_info_local(self):
         self.list_info(self.integrations)
 
-    def test_1_list_info_cloud(self):
-        self.list_info(self.cloud_integrations)
+    # def test_1_list_info_cloud(self):
+    #     self.list_info(self.cloud_integrations)
 
 
     def add_integration(self, _type, integrations):
@@ -79,28 +79,25 @@ class TestDatasources(unittest.TestCase):
 
         integration_params = self.integration_creds[_type]
         integration_params["type"] = _type
-        integration_params["test"] = True
         integration_params["publish"] = False
-
+        integration_params["user"] = "original_user"
         integrations[origin_name] = {"params": integration_params}
         self.assertTrue(isinstance(integrations[origin_name].get_info(), dict))
         self.assertTrue(len(integrations[origin_name]) > 5)
 
-    def update_integration(self, _type, integrations, to_update=None):
+    def update_integration(self, _type, integrations, to_update={'user': 'updated_user'}):
         origin_name = f"{_type}_{self.integration_suffix}"
         integration = integrations[origin_name]
         self.assertTrue(integration is not None)
         update_params = self.integration_creds[_type]
         update_params["type"] = _type
-        # update only one field
-        # to make it private
-        update_params["publish"] = False
+        # update_params["user"] = "updated_user"
         if to_update is not None:
             update_params.update(to_update)
 
         integration.update({"params": update_params})
 
-        self.assertTrue(not integration.get_info()[""])
+        self.assertTrue(isinstance(integration.get_info(), dict))
         if to_update is not None:
             for k in to_update:
                 self.assertTrue(integration.get_info()[k] == to_update[k])
@@ -111,8 +108,8 @@ class TestDatasources(unittest.TestCase):
     # def test_2_clickhouse_cloud(self):
     #     self.add_integration("clickhouse", self.cloud_integrations)
 
-    # def test_3_update_clickhouse_local(self):
-    #     self.update_integration("clickhouse", self.integrations)
+    def test_3_update_clickhouse_local(self):
+        self.update_integration("clickhouse", self.integrations)
 
     # def test_3_update_clickhouse_cloud(self):
     #     self.update_integration("clickhouse", self.cloud_integrations)
@@ -123,8 +120,8 @@ class TestDatasources(unittest.TestCase):
     # def test_4_add_myslq_cloud(self):
     #     self.add_integration("mysql", self.cloud_integrations)
 
-    # def test_5_update_mysql_local(self):
-    #     self.update_integration("mysql", self.integrations)
+    def test_5_update_mysql_local(self):
+        self.update_integration("mysql", self.integrations)
 
     # def test_5_update_mysql_cloud(self):
     #     self.update_integration("mysql", self.cloud_integrations)
@@ -135,8 +132,8 @@ class TestDatasources(unittest.TestCase):
     # def test_6_add_mongo_cloud(self):
     #     self.add_integration("mongodb", self.cloud_integrations)
 
-    # def test_7_update_mongo_local(self):
-    #     self.update_integration("mongodb", self.integrations)
+    def test_7_update_mongo_local(self):
+        self.update_integration("mongodb", self.integrations)
 
     # def test_7_update_mongo_cloud(self):
     #     self.update_integration("mongodb", self.cloud_integrations)
@@ -147,8 +144,8 @@ class TestDatasources(unittest.TestCase):
     # def test_8_add_mariadb_cloud(self):
     #     self.add_integration("mariadb", self.cloud_integrations)
 
-    # def test_9_update_mariadb_local(self):
-    #     self.update_integration("mariadb", self.integrations)
+    def test_9_update_mariadb_local(self):
+        self.update_integration("mariadb", self.integrations)
 
     # def test_9_update_mariadb_cloud(self):
     #     self.update_integration("mariadb", self.cloud_integrations)
@@ -159,20 +156,20 @@ class TestDatasources(unittest.TestCase):
     # def test_10_add_postgres_cloud(self):
     #     self.add_integration("postgres", self.cloud_integrations)
 
-    # def test_11_update_postgres_local(self):
-    #     self.update_integration("postgres", self.integrations)
+    def test_11_update_postgres_local(self):
+        self.update_integration("postgres", self.integrations)
 
     # def test_11_update_postgres_cloud(self):
     #     self.update_integration("postgres", self.cloud_integrations)
 
-    # def test_12_add_snowflake_local(self):
-    #     self.add_integration("snowflake", self.integrations)
+    def test_12_add_snowflake_local(self):
+        self.add_integration("snowflake", self.integrations)
 
     # def test_12_add_snowflake_cloud(self):
     #     self.add_integration("snowflake", self.cloud_integrations)
 
-    # def test_13_update_snowflake_local(self):
-    #     self.update_integration("snowflake", self.integrations, to_update={'test': True})
+    def test_13_update_snowflake_local(self):
+        self.update_integration("snowflake", self.integrations, to_update={'test': True})
 
     # def test_13_update_snowflake_cloud(self):
     #     self.update_integration("snowflake", self.cloud_integrations, to_update={'test': True})
