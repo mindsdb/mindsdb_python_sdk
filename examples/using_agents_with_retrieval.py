67d782746af0b2e249e5756c4af3f3e603c80b36
dusvyat <usvyat@gmail.com>
2024-07-29 09:46:32 +0300

    Add ability to get retrieved contexts

--- examples/using_agents_with_retrieval.py
+++ examples/using_agents_with_retrieval.py
@@ -9,11 +9,14 @@ model_name = 'gpt-4'
 
 # Now create an agent that will use the model we just created.
 agent = con.agents.create(name=f'mindsdb_retrieval_agent_{model_name}_{uuid4().hex}',
-                          model='gpt-4')
+                          model='gpt-4',
+                          params={'return_context': True})
 
 agent.add_file('./data/tokaido-rulebook.pdf', 'rule book for the board game Tokaido')
 
 
 question = "what are the rules for the game takaido?"
 answer = agent.completion([{'question': question, 'answer': None}])
-print(answer.content)
+print(answer.context)
+print(answer)
+
