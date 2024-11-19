a45be4010915a6f86f719c174d1ba954fdb37fc4
Zoran Pandovski <zoran.pandovski@gmail.com>
2024-11-19 11:44:03 +0100

    Update __about__.py

--- mindsdb_sdk/__about__.py
+++ mindsdb_sdk/__about__.py
@@ -6,5 +6,5 @@ __email__ = "jorge@mindsdb.com"
 __author__ = 'MindsDB Inc'
 __github__ = 'https://github.com/mindsdb/mindsdb_python_sdk'
 __pypi__ = 'https://pypi.org/project/mindsdb-sdk/'
-__license__ = 'GPL-3.0'
+__license__ = 'MIT'
 __copyright__ = 'Copyright 2020- mindsdb'
